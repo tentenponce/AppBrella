c9aad7fdc9d7512ef8ec4dc80e1e94e330f740b0
tentenponce <tentenponce@tentenponces-MacBook-Pro.local>
2019-03-05 15:57:51 +0800

    Cleanup

--- app/src/main/java/com/tcorner/appbrella/ui/drawer/DrawerPresenter.kt
+++ app/src/main/java/com/tcorner/appbrella/ui/drawer/DrawerPresenter.kt
@@ -8,5 +8,4 @@ import javax.inject.Inject
  * Created by Exequiel Egbert V. Ponce on 11/4/2018.
  */
 
-class DrawerPresenter @Inject constructor() : BasePresenter<DrawerMvpView>() {
-}
+class DrawerPresenter @Inject constructor() : BasePresenter<DrawerMvpView>()
